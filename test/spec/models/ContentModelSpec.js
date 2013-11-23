115253bd3d38617e3a2c62f31279b05f5273e22d
OpenShift guest <pm222br@student.lnu.se>
2013-11-23 12:59:37 -0500

    Fix

--- test/spec/models/ContentModelSpec.js
+++ test/spec/models/ContentModelSpec.js
@@ -1,11 +1,17 @@
 
 define(['js/models/contentmodel'], function(Model) {
   
-    var model = new Model();
-  
     describe('ContentModel', function() {
-        it('onePlusOneTest()', function() {
+        
+        var model = new Model();
+        
+        it('onePlusOneTest(1, 1)', function() {
             expect(model.onePlusOneTest(1, 1)).toEqual(2);
         });
+        
+        it('onePlusOneTest(1, 100)', function() {
+            expect(model.onePlusOneTest(1, 100)).toEqual(101);
+        });
+
     });
 });
